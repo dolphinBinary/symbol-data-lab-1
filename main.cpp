1fb09dad0f62032099f79f9ab0c4bcfbeb717f13
Dolphin Binary <dolphinbinary@gmail.com>
2023-02-28 11:22:01 +0300

    wip

--- main.cpp
+++ main.cpp
@@ -1,31 +1,68 @@
 #include <iostream>
 
 int main() {
+//    char checkWord[] = "abcd";
+//    char stringWords[] = "abcd aaaa abcd abcd asfg";
+//    char checkWord[] = "asdg";
+//    char stringWords[] = "hsdf sdfh jghj dfgf sgfh";
+//    char checkWord[] = "asdg";
+//    char stringWords[] = "sdff aaaa sdff sdfg sfdg";
+//    char checkWord[] = "asdg";
+//    char stringWords[] = "sdfg agfg sdfg sdfg sfdg";
+//    char checkWord[] = "asdg";
+//    char stringWords[] = "sfss sfdg sgf sdfg avds";
+//    char checkWord[] = "asdg";
+//    char stringWords[] = "asdg aggd asdg afgg asfg";
+//    char checkWord[] = "a";
+//    char stringWords[] = "aggd aaaa fhdj fghj jfghj";
+//    char checkWord[] = "b";
+//    char stringWords[] = "aggd aaaa vjkjk hgjj fhjj";
+//    char checkWord[] = "c";
+//    char stringWords[] = "aggd aaaa aggd aggd aggd";
+//    char checkWord[] = "d";
+//    char stringWords[] = "aggd aaaa about aggd aggd";
+//    char checkWord[] = "asdf";
+//    char stringWords[] = "aggd aaaa aggd aggd aggd";
     char checkWord[] = "test";
-    char stringWords[] = "something words about computer science";
+    char stringWords[] = "something aaaa about computer science";
 
-    int i = 0;
-    int j = 0;
+    int lenCheckWord = 0;
 
-    // Проверяет на наличие букв контрольного слова внутри первого слова строки
-    while (stringWords[i] != '\0')
-    {
-        while (stringWords[i] != ' ' && stringWords[i] != '\0')
-        {
-            while (checkWord[j] != ' ' && stringWords[i] != '\0')
-            {
-                if (checkWord[j] == stringWords[i])
-                {
-                    while(stringWords[i] != ' ' && stringWords[i] != '\0')
-                    {
-                        std::cout << stringWords[i];
-                        i++;
-                    }
+    while (checkWord[lenCheckWord] != '\0')
+        ++lenCheckWord;
+    ++lenCheckWord;
+
+    int wordCounter = 0;
+
+    int begin = 0;
+    int end;
+
+    while (stringWords[begin] != '\0') {
+        while (stringWords[begin] == ' ')
+            ++begin;
+        end = begin + 1;
+        while (stringWords[end] != ' ' && stringWords[end] != '\0')
+            ++end;
+
+        // слово между begin и end
+        bool isLetterInWord = false;
+        for (int i = begin; (i < end) && (!isLetterInWord); ++i)
+            for (int j = 0; j < lenCheckWord; ++j)
+                if (stringWords[i] == checkWord[j]) {
+                    isLetterInWord = true;
+                    break;
                 }
-                j++;
-            }
-        }
+        if (isLetterInWord) {
+            wordCounter += 1;
+            std::cout << "yes\n";
+        } else
+            std::cout << "nope\n";
+        begin = end;
+
+        std::cout << "\n";
+
     }
+    std::cout << "words contains check word " << wordCounter;
 
     return 0;
 }
